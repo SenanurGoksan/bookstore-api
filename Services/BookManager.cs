2ac425483f3db1761c035cc93e42596c0d468cd4
Senanur Goksan <senanurgoksaan@gmail.com>
2025-12-12 22:50:27 +0300

    feat: add filtering

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Entities.Exceptions.BadRequestException;
 
 namespace Services
 {
@@ -44,6 +45,9 @@ namespace Services
 
         public async Task<(IEnumerable<BookDto> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
+            if (!bookParameters.ValidPriceRange)
+                throw new PriceOutofRangeBadRequestException();
+
             var booksWithMetaData = await _manager
                 .Book
                 .GetAllBooksAsync(bookParameters,trackChanges);
